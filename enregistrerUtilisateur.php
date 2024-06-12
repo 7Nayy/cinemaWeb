1dd9e93533c87c5747f6f88884fdab992dfde811
7Nayy <nadim.benkrouidem@gmail.com>
2024-06-12 11:53:48 +0200

    ok

--- enregistrerUtilisateur.php
+++ enregistrerUtilisateur.php
@@ -2,7 +2,7 @@
 include 'config.php';
 
 $username = $_POST['username'];
-$password = md5($_POST['password']);
+$password = password_hash($_POST['password'], PASSWORD_BCRYPT);
 
 $stmt = $pdo->prepare('INSERT INTO Utilisateur (Nom, MotDePasse) VALUES (:username, :password)');
 $stmt->execute(['username' => $username, 'password' => $password]);
